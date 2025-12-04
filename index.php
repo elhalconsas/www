57c98405b0dae6928ef14d17749e786f2b3e1082
elhalconsas <samyabreu2005@gmail.com>
2025-12-04 16:24:41 -0500

    new file:   busqueda/busqueda1.php 	new file:   busqueda/busqueda2.php 	new file:   cliente/cliente.php 	new file:   cliente/cliente_delete.php 	new file:   cliente/cliente_insert.php 	new file:   cliente/cliente_select.php 	new file:   config/conexion.php 	new file:   consultas/consulta1.php 	new file:   consultas/consulta2.php 	new file:   dbEjemplo.txt 	new file:   empresa/empresa.php 	new file:   empresa/empresa_delete.php 	new file:   empresa/empresa_insert.php 	new file:   empresa/empresa_select.php 	new file:   includes/footer.php 	new file:   includes/header.php 	new file:   index.php 	new file:   modelo.png 	new file:   proyecto/proyecto.php 	new file:   proyecto/proyecto_delete.php 	new file:   proyecto/proyecto_insert.php 	new file:   proyecto/proyecto_select.php

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trabajo final BD1</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+
+<body>
+<div class="container mt-5">
+
+    <h1 class='fw-bold'>Trabajo Final BD1 - (Título de su trabajo)</h1>
+
+    <div class="informacion">
+        <ul>
+            <li><b>Materia:</b> Bases de Datos I</li>
+            <li><b>Profesor:</b> Francisco Javier Moreno Arboleda</li>
+            <li><b>Institución:</b> Universidad Nacional de Colombia Sede Medellín</li>
+            <li><b>Semestre:</b> 2025-2</li>
+        </ul>
+    </div>
+
+    <div class="integrantes">
+        <h2 class='fw-bold'>Integrantes</h2>
+        <ul>
+            <li>Samuel Abreu Velez</li>
+            <li>Manuel Restrepo Sierra</li>
+            <li>Integrante 3</li>
+        </ul>
+    </div>
+
+    <div class="modelo">
+        <h2 class='fw-bold'>Modelo E-R (Poner su modelo análogo)</h2>
+        <p></p>
+    <img src="modelo.png" class="img-fluid" alt="Modelo E-R">
+    </div>
+
+    <div class="enlaces">
+        <h2 class='fw-bold'>Enlaces</h2>
+        <ul>
+            <li class="mb-3"><a class="btn btn-success" role="button" href="cliente/cliente.php">Entidad análoga a TORNEO</a></li>
+            <li class="mb-3"><a class="btn btn-success" role="button" href="empresa/empresa.php">Entidad análoga a EUIPO</a></li>
+            <li class="mb-3"><a class="btn btn-success" role="button" href="proyecto/proyecto.php">Entidad análoga a VIDEOJUEGO</a></li>
+            <li class="mb-3"><a class="btn btn-primary" href="consultas/consulta1.php">Consulta 1</a></li>
+            <li class="mb-3"><a class="btn btn-primary" href="consultas/consulta2.php">Consulta 2</a></li>
+            <li class="mb-3"><a class="btn btn-info" role="button" href="busqueda/busqueda1.php">Búsqueda 1</a></li>
+            <li class="mb-3"><a class="btn btn-info" role="button" href="busqueda/busqueda2.php">Búsqueda 2</a></li>
+        </ul>
+    </div>
+</div>
+</body>
+
+</html>
